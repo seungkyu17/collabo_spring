51b6951b47bf6222aa5542e401ec6e02e0b34118
seungkyu17 <seungkyu17@naver.com>
2025-10-10 16:56:40 +0900

    2025/09/197

--- src/main/java/com/coffee/service/ProductService.java
+++ src/main/java/com/coffee/service/ProductService.java
@@ -3,6 +3,8 @@ package com.coffee.service;
 import com.coffee.entity.Product;
 import com.coffee.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -55,5 +57,9 @@ public class ProductService {
             return productRepository.findByImageContaining(filter);
         }
         return productRepository.findAll();
+    }
+
+    public Page<Product> listProducts(Pageable pageable) {
+        return this.productRepository.findAll(pageable);
     }
 }
